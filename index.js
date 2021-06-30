09fca13c047e2125231730cb445ed82e9880f5e7
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-29 20:46:12 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const counter = document.getElementById("num");
+const inc = document.querySelector(".inc");
+const dec = document.querySelector(".dec");
+
+let cnt = 0;
+
+inc.addEventListener("click", () => {
+  cnt++;
+  counter.innerHTML = cnt;
+});
+
+dec.addEventListener("click", () => {
+  cnt--;
+  counter.innerHTML = cnt;
+});
